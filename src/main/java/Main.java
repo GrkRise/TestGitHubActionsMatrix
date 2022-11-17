9bf9e4a94bd29696a587712437f8098d0d82ef42
LenaDutova <lena.dutova@list.ru>
2022-11-17 18:21:58 +0300

    update gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int add(int x, int y){
+        return x + y;
+    }
+
+    public int diff(int x, int y){
+        return x - y;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    Main main = new Main();
+
+    @Test
+    void additional(){
+        assertEquals(2, main.add(1, 1));
+    }
+
+    @Test
+    void difference(){
+        assertEquals(1, main.diff(3, 2));
+    }
+}
+
+
